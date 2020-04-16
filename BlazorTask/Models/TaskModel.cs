ed09bbb6dd93565e6eb33bf732690f1f7e1d86b5
Sunil Anthony <sunil.anthony@stellarmls.com>
2020-04-16 16:47:33 -0400

    Initial Commit

---
+++ BlazorTask/Models/TaskModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorTask.Models
+{
+    public class TaskModel
+    {
+        public string Task { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
